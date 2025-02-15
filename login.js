8b38ab7b5e00acf0c0223c1e32d571d3dc2b4f82
sunstone-porwal <love.porwal@sunstone.in>
2025-02-16 00:20:38 +0530

    login code added

--- login.js
+++ login.js
@@ -1,3 +1,14 @@
 let login_email = "l@gmail.com"
 
 let login_pass ="12345"
+
+let enter_email ="l@gmail.com"
+
+let enter_pass = "12345";
+
+
+if(login_email==enter_email && login_pass==enter_pass){
+    console.log("login successfull")
+} else{
+    console.log("login failed")
+}
